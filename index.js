a8d9ba41c68fa1f8f397d90ed41eaaeb7ac2e01f
Arun Kumar <arunkumar.krishna@gmail.com>
2017-01-23 18:16:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,5 +18,6 @@ nightmare.on('did-get-response-details',function () {
 nightmare.goto("https://www.flipkart.com/philips-qt4011-15-pro-skin-advanced-trimmer-men/p/itmdu7ymgsjzza2j?pid=SHVDU7YMGSJZZA2J&fm=merchandising&iid=M_eccf6eb3-ffc8-4377-a612-9fbe3baee451.c3fb6983-fb27-40c0-bccd-7bef0bfc288c&otracker=hp_omu_Deals+of+the+Day_1_Just+%E2%82%B91%2C549_c3fb6983-fb27-40c0-bccd-7bef0bfc288c_0")
     .then(function () {
        console.log("In Page");
+       nightmare.end();
 });
 
