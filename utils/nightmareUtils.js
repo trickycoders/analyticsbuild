eb336dafd0a02431e6fbd9d5ad3c6d019f81705a
Arun Kumar <arunkumar.krishna@gmail.com>
2017-02-06 16:20:10 +0530

    Added the test

--- utils/nightmareUtils.js
+++ utils/nightmareUtils.js
@@ -1,7 +1,7 @@
 const Nightmare = require('nightmare');
 
 module.exports.getNightmareInstance = function(type){
-    return Nightmare({
+    let options = {
         show: false,
         openDevTools: true,
         width: 1280,
@@ -9,5 +9,10 @@ module.exports.getNightmareInstance = function(type){
         webPreferences: {
             partition: 'nopersist'
         }
-    });
+    };
+    if(type && type === "MOBILE"){
+        options.width =  360;
+        options.height =  640;
+    }
+    return Nightmare(options);
 }
